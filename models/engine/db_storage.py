ba7b00c7cea2a13e6101ce9ade5f66f9ac6f60e1
Razika-Bengana <5003@holbertonstudents.com>
2022-11-25 23:10:44 +0100

    update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -21,47 +21,70 @@ class DBStorage:
 
     def __init__(self):
         """initializes the storage"""
-        usr = getenv('HBNB_MYSQL_USER')
-        pwd = getenv('HBNB_MYSQL_PWD')
-        host = getenv('HBNB_MYSQL_HOST')
-        db = getenv('HBNB_MYSQL_DB')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            usr, pwd, host, db), pool_pre_ping=True)
-        if getenv('HBNB_ENV') == 'test':
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
+            getenv("HBNB_MYSQL_USER"),
+            getenv("HBNB_MYSQL_PWD"),
+            getenv("HBNB_MYSQL_HOST"),
+            getenv("HBNB_MYSQL_DB"),
+            pool_pre_ping=True
+        ))
+
+        if getenv("HBNB_ENV") == "test":
+            from models.base_model import Base
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Returns a dictionaary of all objects in DB"""
-        objs = {}
-        if cls in mapped_classes:
-            objs.update({"{}.{}".format(cls.__name__, item.id): item
-                         for item in self.__session.query(cls)})
-        elif cls is None:
-            for c in mapped_classes:
-                objs.update({"{}.{}".format(c.__name__, item.id): item
-                             for item in self.__session.query(c)})
-        return objs
+        """retrives all objects of a class name"""
+        from models.base_model import Base
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.user import User
+        from models.state import State
+        new_dict = {}
+        all_class = [City, State, User, Place, Review, Amenity]
+        list_objects = []
+        if cls is None:
+            for i in range(len(all_class)):
+                list_objects += self.__session.query(all_class[i]).all()
+        else:
+            list_objects += self.__session.query(cls).all()
+
+        for element in list_objects:
+            key = "{}.{}".format(element.__class__.__name__, element.id)
+            new_dict[key] = element
+        return new_dict
 
     def new(self, obj):
-        """adds an object to the session"""
-        if type(obj) in mapped_classes:
-            self.__session.add(obj)
+        """adds the object to the current database session"""
+        self.__session.add(obj)
 
     def save(self):
-        """commits the changes in  the current session"""
+        """commit all changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """deletes an object from the session"""
-        if obj in self.all(type(obj).values()):
+        """delete from the current database session obj if not None"""
+        if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """reloads from the database"""
+        """create all tables in the database and the current
+           database session"""
+        from models.base_model import Base
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.user import User
+        from models.state import State
         Base.metadata.create_all(self.__engine)
-        self.__session = scoped_session(sessionmaker(bind=self.__engine,
-                                                     expire_on_commit=False))
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
 
-    def close(self):
-        """closes the current session"""
-        self.__session.remove()
+        def close(self):
+            """remove method: remove the session"""
+            self.__session.close()
