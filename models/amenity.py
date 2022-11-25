6f5221128aac707891eddd0089e50bbc1317674c
Razika-Bengana <5003@holbertonstudents.com>
2022-11-25 22:50:06 +0100

    reinstate amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from models.base_model import BaseModel
 
 
 class Amenity(BaseModel, Base):
     """the class Amenity"""
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
+    name = ""
