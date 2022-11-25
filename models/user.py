3738d2dc4c13b5ba4d7b71a7645d25a216727248
Razika-Bengana <5003@holbertonstudents.com>
2022-11-26 00:05:33 +0100

    update user.py

--- models/user.py
+++ models/user.py
@@ -7,8 +7,7 @@ from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
     """Representation of a user """
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
