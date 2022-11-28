abbbe82680ff86608227c097cab1e469333a2c0a
Elizabeh <eliz01_01@yahoo.com>
2022-11-28 15:23:29 +0100

    Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/bin/usr/python3
+'''script that starts a Flask web application'''
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    '''Method displays'''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    '''method displays
+    '''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cisfun(text):
+    '''method replace and displays
+    '''
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    '''method replace and displays
+    '''
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
