1c07f6a620e07b0fcdb6cd5fc0611bb2a0ceca98
Elizabeh <eliz01_01@yahoo.com>
2022-11-30 16:01:37 +0100

    HBNB filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''Start a Flask web app listening on 0.0.0.0 port 5000'''
+from flask import Flask, render_template
+from models import storage
+from models.amenity import Amenity
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(execption):
+    '''
+    '''
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def filter():
+    '''
+    '''
+    list_state = storage.all(State).values()
+    list_amenities = storage.all(Amenity).values()
+    return render_template('10-hbnb_filters.html', list_state=list_state,
+                           list_amenities=list_amenities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
